5b062211d6c6a7a6d02f0e123cc3e6c7c2fd4c1b
Matthew Miley <mmiley@technicalassurance.com>
2019-05-03 15:51:07 -0400

    intial setup

---
+++ script.py
@@ -0,0 +1,18 @@
+
+#Create virtual environment
+#C:\Python37\python.exe -m venv venv
+
+#OR Create virtual environment with global packages  #Not common
+#C:\Python37\python.exe -m venv venv --system-site-packages
+
+#Activate virtual enviroment
+#venv\Scripts\activate.bat
+
+#Install requirements
+#pip install -r requirements.txt
+
+#List all libs installed
+#pip list
+
+#List all libs for creating a requirements file
+#pip freeze
